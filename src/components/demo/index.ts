56969283367c9ecd966264490ccf411f52811a0b
Drey <djlan508yt@gmail.com>
2026-09-14 05:34:08 +0000

    chore: empty repository completely as requested

--- src/components/demo/index.ts
+++
@@ -1,5 +0,0 @@
-export { DemoContainer } from "./demo-container";
-export { DemoTerminal } from "./demo-terminal";
-export { DemoDashboard } from "./demo-dashboard";
-export { DemoVisual } from "./demo-visual";
-export { DemoForm } from "./demo-form";
--- src/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export { useMediaQuery } from "./use-media-query";
-export { useIntersectionObserver } from "./use-intersection-observer";
-export { useScrollProgress } from "./use-scroll-progress";
-export { useMousePosition } from "./use-mouse-position";
